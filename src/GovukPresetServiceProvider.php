eea2e26f4bde2e37e88514abd2f905329a4434b9
Luke Vincent <luke.vincent@me.com>
2020-02-06 16:43:50 +0000

    Apply fixes from StyleCI

--- src/GovukPresetServiceProvider.php
+++ src/GovukPresetServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace GovukPreset;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
+use Illuminate\Support\ServiceProvider;
 
 class GovukPresetServiceProvider extends ServiceProvider
 {
@@ -14,12 +14,12 @@ class GovukPresetServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        PresetCommand::macro('govuk', function($command){
+        PresetCommand::macro('govuk', function ($command) {
             Preset::install(false);
             $command->info('govuk-frontend scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
-        PresetCommand::macro('govuk-auth', function($command){
+        PresetCommand::macro('govuk-auth', function ($command) {
             Preset::install(true);
             $command->info('govuk-frontend scaffolding with Auth views installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
